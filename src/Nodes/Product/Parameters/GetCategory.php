cb23dc1dc426fbd545af8ef836a9c8cdf9d855f1
nda666 <adhabakhtiar@gmail.com>
2022-06-23 08:13:19 +0700

    fix lint

--- src/Nodes/Product/Parameters/GetCategory.php
+++ src/Nodes/Product/Parameters/GetCategory.php
@@ -12,7 +12,13 @@ class GetCategory extends RequestParameters
     }
 
      /**
-     * If language is not uploaded, the default language=en, the following are the languages supported by different markets SG: en ; MY: en / ms-my / zh-hans ; TH: en / th ; VN: en / vi ; PH: en ; TW: en / zh-hant ; ID: en / id ; BR: en / pt-br ; MX: en / es-mx ; PL: pl ; CO: en/es-CO ; CL: en/es-CL ; FR: en/fr ; ES: en/es-ES ; AR:en / es-ar Note: For markets that have already launched global tree, Crossboard shop only support returning en and zh-hans language data
+     * If language is not uploaded, the default language=en, the following are
+     *  the languages supported by different markets SG: en ; MY: en / ms-my /
+     *  zh-hans ; TH: en / th ; VN: en / vi ; PH: en ; TW: en / zh-hant ; ID:
+     *  en / id ; BR: en / pt-br ; MX: en / es-mx ; PL: pl ; CO: en/es-CO ; CL:
+     *  en/es-CL ; FR: en/fr ; ES: en/es-ES ; AR:en / es-ar Note: For markets
+     *  that have already launched global tree, Crossboard shop only support
+     *  returning en and zh-hans language data
      *
      * @param string $name
      * @return $this
